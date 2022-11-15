bb3d250e1a7407c775488e925f4a0a1b09c644fc
Caleb <fishfilli@outlook.com>
2022-11-14 17:21:07 -0800

    Add getComputerChoice()

---
+++ script.js
@@ -0,0 +1,13 @@
+function getComputerChoice() {
+    const options = ["rock", "paper", "scissors"];  
+    return options[Math.floor(Math.random() * options.length)];
+}
+
+function getPlayerChoice() {
+
+}
+
+
+function playRound(playerSelection, computerSelection) {
+    
+}
