86be06f0ab9e972309d296a58e22dc4201144f4b
Caleb <fishfilli@outlook.com>
2022-11-18 14:29:43 -0800

    Remove getPlayerChoice prompt

--- script.js
+++ script.js
@@ -3,12 +3,6 @@ function getComputerChoice() {
     return options[Math.floor(Math.random() * options.length)];
 }
 
-function getPlayerChoice() {
-    let playerChoice = prompt("Please choose rock, paper, or scissors.");
-    return playerChoice;
-}
-
-
 function playRound(playerSelection, computerSelection) {
     console.log("start of playround");
     if (playerSelection === computerSelection) {
